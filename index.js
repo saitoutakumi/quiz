4f2c83f324d72ad31691f4275985f612bea366f0
斉藤拓海 <saitoutakumi@saitoutakuminoMacBook-Air.local>
2023-01-18 17:23:50 +0900

    1/18

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
-// class User {
-//   constructor(name, age) {
-//     this.name = name;
-//     this.age = age;
-//   }
-// }
-// const user1 = new User("bob", 23);
-// console.log("user1", user1);
-// console.log("user1", user1.age);
-
 const answer = document.getElementById("answer");
 
-fetch("https://opentdb.com/api.php?amount=10")
-  .then((res) => {
-    return res.json();
-  })
-
-  .then((results) => {
-    ShowQuiz(results);
-  })
-
-  .catch((error) => console.log(error));
-
 class Quiz {
-  // Quizというクラス
   constructor(quizData) {
     this.quizData = quizData;
   }
@@ -32,31 +10,108 @@ class Quiz {
   getType(num) {
     return this.quizData[num].type;
   }
+  getDifficulty(num) {
+    return this.quizData[num].difficulty;
+  }
+  getQuestion(num) {
+    return this.quizData[num].question;
+  }
+  getCorrect_answer(num) {
+    return this.quizData[num].correct_answer;
+  }
+  getIncorrect_answers(num) {
+    return this.quizData[num].incorrect_answers;
+  }
 }
 
 let num = 0;
-const ShowQuiz = (quizes) => {
-  const quiz = quizes.results;
-  const quiz10 = new Quiz(quiz);
-  const category = quiz10.getCategory(num);
-  const type = quiz10.getType(num);
-  answer.addEventListener("click", () => {
-    console.log(quiz10);
-    if (num < 10) {
-      num++;
-      ShowQuiz(quizes);
-      console.log("category:", category);
-      console.log("type:", type);
-      console.log(num);
-    } else {
-      console.log("finish!!");
-    }
+
+fetch("https://opentdb.com/api.php?amount=10")
+  .then((res) => {
+    return res.json();
+  })
+  .then((res) => {
+    console.log(res);
+    // 10問分で初期化
+    const quiz = new Quiz(res.results);
+    const category = quiz.getCategory(num);
+    const type = quiz.getType(num);
+    const diffculty = quiz.getDifficulty(num);
+    const question = quiz.getQuestion(num);
+    const correct_answer = quiz.getCorrect_answer(num);
+    const incorrect_answers = quiz.getIncorrect_answers(num);
+    // クリックされるたびに、showQuizを実行させて、クイズを一問ずつ表示する
+    showQuiz(
+      category,
+      type,
+      diffculty,
+      question,
+      correct_answer,
+      incorrect_answers
+    );
+    answer.addEventListener("click", () => {
+      showQuiz(
+        quiz.getCategory(num),
+        quiz.getType(num),
+        quiz.getDifficulty(num),
+        quiz.getQuestion(num),
+        quiz.getCorrect_answer(num),
+        quiz.getIncorrect_answers(num)
+      );
+    });
   });
+
+const showQuiz = (
+  category,
+  type,
+  difficulty,
+  question,
+  correct_answer,
+  incorrect_answers
+) => {
+  const quizNu = document.getElementById("num");
+  quizNu.innerHTML = "問題" + [num + 1];
+
+  const quizCa = document.getElementById("category");
+  quizCa.innerHTML = "「ジャンル」" + category;
+
+  const quizDi = document.getElementById("difficulty");
+  quizDi.innerHTML = "「難易度」" + difficulty;
+
+  const quizQu = document.getElementById("question");
+  quizQu.innerHTML = question;
+
+  const quizCo_a = document.getElementById("correct_answer");
+  quizCo_a.innerHTML = correct_answer;
+
+  incorrect_answers.forEach((incorrect_answers) => {
+    // const incorrect_answers = is;
+    const quizIn_a = document.getElementById("incorrect_answers");
+    quizIn_a.innerHTML = incorrect_answers;
+    console.log(incorrect_answers);
+  });
+
+  num++;
+
+  // console.log("start showQuiz()");
+  // console.log(`${num + 1}問目のデータ`);
+  // console.log(category);
+  // console.log(type);
+  // console.log(diffculty);
+  // console.log(question);
+  // console.log(correct_answer);
+  // console.log(incorrect_answers);
 };
 
-//なんかで一つずつクイズを表示させる必要がありそう？？
-
-//ワンクリックで複数クイズが表示されてしまう
-//試しにconst category = quiz10.getCategory(4);
-//などと記載しても５問目のクイズが表示される。
-//→任意のクイズナンバーを受け取っている
+// 「ようこそ」「ボタンをクリックしてください」
+// クリックしたら画面の切り替え(APIを取得→クリックイベントにする？)
+// ボタンを押したら「取得中」「少々お持ちください」を表示（APIを取得）
+// クイズ十問分を取得
+// 取得したら一問目の詳細を表示
+// 選択肢をボタンで表示（オブジェクト内では、「正解」と「不正解」で存在）
+// 並びをランダムにする必要あり
+// 配列「不正解」の選択肢を一つずつボタンで出力
+// 選択肢のボタンがクリックされたら次の問題へ
+// 何回正解がクリックされたか取得
+// 十問解答したら、終了、正解数を表示
+// 「ホームに戻る」で最初に戻る
