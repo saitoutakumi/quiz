0f0a20ed120691e9deafa9781d82dc0b8cd1c3c3
斉藤拓海 <saitoutakumi@saitoutakuminoMacBook-Air.local>
2023-01-29 15:58:11 +0900

    1/29

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const answer = document.getElementById("answer");
+const top = document.getElementById("top");
 
 class Quiz {
   constructor(quizData) {
@@ -24,122 +25,112 @@ class Quiz {
   }
 }
 
-let num = 0;
-let co_n = 0;
+const title = document.createElement("div");
+top.appendChild(title);
+title.innerHTML = "ようこそ";
+const s_button = document.createElement("button");
+top.appendChild(s_button);
+s_button.innerHTML = "スタート";
 
-fetch("https://opentdb.com/api.php?amount=10")
-  .then((res) => {
-    return res.json();
-  })
-  .then((res) => {
-    console.log(res);
-    // 10問分で初期化
-    const quiz = new Quiz(res.results);
-    const category = quiz.getCategory(num);
-    const type = quiz.getType(num);
-    const diffculty = quiz.getDifficulty(num);
-    const question = quiz.getQuestion(num);
-    const correct_answer = quiz.getCorrect_answer(num);
-    const incorrect_answers = quiz.getIncorrect_answers(num);
+s_button.addEventListener("click", () => {
+  let num = 0;
+  let co_n = 0;
 
-    // クリックされるたびに、showQuizを実行させて、クイズを一問ずつ表示する
-    showQuiz(
-      category,
-      type,
-      diffculty,
-      question,
-      correct_answer,
-      incorrect_answers
-    );
-    answer.addEventListener("click", () => {
-      if (num < 10) {
-        showQuiz(
-          quiz.getCategory(num),
-          quiz.getType(num),
-          quiz.getDifficulty(num),
-          quiz.getQuestion(num),
-          quiz.getCorrect_answer(num),
-          quiz.getIncorrect_answers(num),
-          creatbutton(incorrect_answers, correct_answer)
-        );
-      } else {
-        console.log("正解数", co_n);
-      }
+  fetch("https://opentdb.com/api.php?amount=10")
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      console.log(res);
+      // 10問分で初期化
+      const quiz = new Quiz(res.results);
+      const category = quiz.getCategory(num);
+      const type = quiz.getType(num);
+      const diffculty = quiz.getDifficulty(num);
+      const question = quiz.getQuestion(num);
+      const correct_answer = quiz.getCorrect_answer(num);
+      const incorrect_answers = quiz.getIncorrect_answers(num);
+
+      // クリックされるたびに、showQuizを実行させて、クイズを一問ずつ表示する
+      showQuiz(
+        category,
+        type,
+        diffculty,
+        question,
+        correct_answer,
+        incorrect_answers
+      );
+      answer.addEventListener("click", () => {
+        if (num < 10) {
+          showQuiz(
+            quiz.getCategory(num),
+            quiz.getType(num),
+            quiz.getDifficulty(num),
+            quiz.getQuestion(num),
+            quiz.getCorrect_answer(num),
+            quiz.getIncorrect_answers(num),
+            creatbutton(incorrect_answers, correct_answer)
+          );
+        } else {
+          console.log("正解数", co_n);
+        }
+      });
     });
-  });
 
-// ？？なぜ下記でダメだったのか(not defined）？？
-// const MakeChoices2 = (incorrect_answers, correct_answer) => {
-//   const choices2 = incorrect_answers.concat(correct_answer);
-//   console.log("練習", choices2);
-// };
-// MakeChoices2(incorrect_answers, correct_answer);
+  // 配列をランダムにする関数
+  const shuffleArray = (incorrect_answers, correct_answer) => {
+    const choices = incorrect_answers.concat(correct_answer);
+    for (let i = choices.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = choices[i];
+      choices[i] = choices[j];
+      choices[j] = temp;
+    }
+  };
 
-// 選択肢ボタンを生成する関数
-// ??生成したボタンの間を離すにはどうしたら？？
-// ??選択肢が変な出方する。（消えない）??
-// ???ではなぜ選択肢だけ残ってしまう？問題文等は毎回消える？？
-
-const shuffleArray = (incorrect_answers, correct_answer) => {
-  const choices = incorrect_answers.concat(correct_answer);
-  for (let i = choices.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    const temp = choices[i];
-    choices[i] = choices[j];
-    choices[j] = temp;
-  }
-};
-
-const creatbutton = (incorrect_answers, correct_answer) => {
-  answer.innerHTML = "";
-  const choices = incorrect_answers.concat(correct_answer);
-  shuffleArray(choices);
-  choices.forEach((choice, index) => {
-    const Cho_button = document.createElement("div");
-    answer.appendChild(Cho_button);
-    const Cho_button2 = document.createElement("button");
-    Cho_button.appendChild(Cho_button2);
-    Cho_button2.innerHTML = choice;
-    // console.log(index, choice);
-    Cho_button2.style.backgroundColor = "lightgray";
-    Cho_button2.addEventListener("click", () => {
-      if (Cho_button2.innerHTML === correct_answer) {
-        co_n++;
-        console.log("正解数", co_n);
-      }
+  // 選択肢ボタンを表示する
+  const creatbutton = (incorrect_answers, correct_answer) => {
+    answer.innerHTML = "";
+    const choices = incorrect_answers.concat(correct_answer);
+    shuffleArray(choices);
+    choices.forEach((choice, index) => {
+      const Cho_button = document.createElement("div");
+      answer.appendChild(Cho_button);
+      const Cho_button2 = document.createElement("button");
+      Cho_button.appendChild(Cho_button2);
+      Cho_button2.innerHTML = choice;
+      // console.log(index, choice);
+      Cho_button2.style.backgroundColor = "lightgray";
+      Cho_button2.addEventListener("click", () => {
+        if (Cho_button2.innerHTML === correct_answer) {
+          co_n++;
+          console.log("正解数", co_n);
+        }
+      });
     });
-  });
-};
+  };
 
-const showQuiz = (
-  category,
-  type,
-  difficulty,
-  question,
-  correct_answer,
-  incorrect_answers,
-  choices
-) => {
-  const quizNu = document.getElementById("num");
-  quizNu.innerHTML = "問題" + [num + 1];
-  const quizCa = document.getElementById("category");
-  quizCa.innerHTML = "「ジャンル」" + category;
-  const quizDi = document.getElementById("difficulty");
-  quizDi.innerHTML = "「難易度」" + difficulty;
-  const quizQu = document.getElementById("question");
-  quizQu.innerHTML = question;
-  creatbutton(incorrect_answers, correct_answer, choices);
-  num++;
-
-  //   console.log("start showQuiz()");
-  //   console.log(`${num + 1}問目のデータ`);
-  //   console.log(category);
-  //   console.log(type);
-  //   console.log(difficulty);
-  //   console.log(question);
-  //   console.log(correct_answer);
-  //   console.log(incorrect_answers);
-};
+  const showQuiz = (
+    category,
+    type,
+    difficulty,
+    question,
+    correct_answer,
+    incorrect_answers,
+    choices
+  ) => {
+    const quizNu = document.getElementById("top");
+    quizNu.innerHTML = "問題" + [num + 1];
+    const quizCa = document.getElementById("category");
+    quizCa.innerHTML = "「ジャンル」" + category;
+    const quizDi = document.getElementById("difficulty");
+    quizDi.innerHTML = "「難易度」" + difficulty;
+    const quizQu = document.getElementById("question");
+    quizQu.innerHTML = question;
+    creatbutton(incorrect_answers, correct_answer, choices);
+    num++;
+  };
+});
 
 // 「ようこそ」「ボタンをクリックしてください」
 // クリックしたら画面の切り替え(APIを取得→クリックイベントにする？)
